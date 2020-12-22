c9be7444e5353faaa46c2927d11834eb94f11b22
Luis Javier Paz <luis.j.paz@unl.edu.es>
2020-12-22 18:03:32 -0500

    primer commit

---
+++ src/main/java/com/MyVieews/MyVieewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.MyVieews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVieewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVieewsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyVieews/MyVieewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyVieews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVieewsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
